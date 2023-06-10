48759ff28c6326fc47b923f2f8442b5c8e529fae
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-11 02:36:30 +0800

    YT Match 3 P19 - Refactoring

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -54,8 +54,13 @@ func _ready():
 
 func restricted_movement(place):
 	# Check the empty pieces
-	for i in empty_spaces.size():
-		if empty_spaces[i] == place:
+	if is_in_array(empty_spaces, place):
+		return true
+	return false
+
+func is_in_array(array, item):
+	for i in array.size():
+		if array[i] == item:
 			return true
 	return false
 
@@ -178,24 +183,28 @@ func find_matches():
 			if all_pieces[i][j] != null:
 				var current_color = all_pieces[i][j].color;
 				if i > 0 && i < width - 1:
-					if all_pieces[i - 1][j] != null && all_pieces[i + 1][j] != null:
+					if !is_piece_null(i - 1, j) && all_pieces[i + 1][j] != null:
 						if all_pieces[i - 1][j].color == current_color && all_pieces[i + 1][j].color == current_color:
-							all_pieces[i - 1][j].matched = true;
-							all_pieces[i - 1][j].dim();
-							all_pieces[i][j].matched = true;
-							all_pieces[i][j].dim();
-							all_pieces[i + 1][j].matched = true;
-							all_pieces[i + 1][j].dim();
+							match_and_dim(all_pieces[i - 1][j])
+							match_and_dim(all_pieces[i][j])
+							match_and_dim(all_pieces[i + 1][j])
 				if j > 0 && j < height - 1:
 					if all_pieces[i][j - 1] != null && all_pieces[i][j + 1] != null:
 						if all_pieces[i][j - 1].color == current_color && all_pieces[i][j + 1].color == current_color:
-							all_pieces[i][j - 1].matched = true;
-							all_pieces[i][j - 1].dim();
-							all_pieces[i][j].matched = true;
-							all_pieces[i][j].dim();
-							all_pieces[i][j + 1].matched = true;
-							all_pieces[i][j + 1].dim();
+							match_and_dim(all_pieces[i][j - 1])
+							match_and_dim(all_pieces[i][j])
+							match_and_dim(all_pieces[i][j + 1])
 	get_parent().get_node("destory_timer").start();
+
+func is_piece_null(column, row):
+	if all_pieces[column][row] == null:
+		return true
+	return false
+
+
+func match_and_dim(item):
+	item.matched = true
+	item.dim()
 
 func destroy_matched():
 	var was_matched = false
