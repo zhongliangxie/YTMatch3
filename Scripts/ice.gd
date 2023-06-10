12a3f5f68b2b165c84ed84eb1cde96a321179279
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-11 02:19:47 +0800

    YT Match 3 P18

---
+++ Scripts/ice.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+export (int) var health
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func take_damage(damage):
+	health -= damage
+	 # Can add damage effect here
