f546617945b0673882fba1798e4a9e4eb094a8cb
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-10 13:59:23 +0800

    YT Match 3 P14

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -6,6 +6,7 @@ export (int) var height;
 export (int) var x_start;
 export (int) var y_start;
 export (int) var offset;
+export (int) var y_offset;
 
 # The piece array
 var possible_pieces = [
@@ -183,7 +184,8 @@ func refill_columns():
 				# Instance that piece from the array
 
 				add_child(piece);
-				piece.position = grid_to_pixel(i, j);
+				piece.position = grid_to_pixel(i, j - y_offset);
+				piece.move(grid_to_pixel(i, j))
 				all_pieces[i][j] = piece;
 
 func _on_destory_timer_timeout():
