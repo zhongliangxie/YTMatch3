91d4788b7365f72d5963149887ebb33498eab396
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-11 06:36:51 +0800

    YT Match 3 P21 - "Icing" tiles

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -16,12 +16,15 @@ export (int) var y_offset;
 export (PoolVector2Array) var empty_spaces
 export (PoolVector2Array) var ice_spaces
 export (PoolVector2Array) var lock_spaces
+export (PoolVector2Array) var concrete_spaces
 
 # Obstacle Signals
 signal make_ice
 signal damage_ice
 signal make_lock
 signal damage_lock
+signal make_concrete
+signal damage_concrete
 
 # The piece array
 var possible_pieces = [
@@ -55,16 +58,21 @@ func _ready():
 	spawn_pieces();
 	spawn_ice()
 	spawn_locks()
+	spawn_concrete()
 
 func restricted_fill(place):
 	# Check the empty pieces
 	if is_in_array(empty_spaces, place):
+		return true
+	if is_in_array(concrete_spaces, place):
 		return true
 	return false
 
 func restricted_move(place):
 	# Check the licorice pieces
 	if is_in_array(lock_spaces, place):
+		return true
+	if is_in_array(concrete_spaces, place):
 		return true
 	return false
 
@@ -73,6 +81,11 @@ func is_in_array(array, item):
 		if array[i] == item:
 			return true
 	return false
+
+func remove_from_array(array, item):
+	for i in range(array.size() - 1, -1, -1):
+		if array[i] == item:
+			array.remove(i)
 
 func make_2d_array():
 	var array = [];
@@ -108,6 +121,10 @@ func spawn_ice():
 func spawn_locks():
 	for i in lock_spaces.size():
 		emit_signal("make_lock", lock_spaces[i])
+
+func spawn_concrete():
+	for i in concrete_spaces.size():
+		emit_signal("make_concrete", concrete_spaces[i])
 
 func match_at(i, j, color):
 	if i > 1:
@@ -236,9 +253,23 @@ func destroy_matched():
 	else:
 		swap_back()
 
+func check_concrete(column, row):
+	# Check Right
+	if column < width - 1:
+		emit_signal("damage_concrete", Vector2(column + 1, row))
+	# Check Left
+	if column > 0:
+		emit_signal("damage_concrete", Vector2(column - 1, row))
+	# Check up
+	if row < height - 1:
+		emit_signal("damage_concrete", Vector2(column, row + 1))
+	if row > 0:
+		emit_signal("damage_concrete", Vector2(column, row - 1))
+
 func damage_special(column, row):
 	emit_signal("damage_ice", Vector2(column, row))
-	emit_signal("damage_lock", Vector2(column, row))	
+	emit_signal("damage_lock", Vector2(column, row))
+	check_concrete(column, row)
 
 func collapse_columns():
 	for i in width:
@@ -294,6 +325,7 @@ func _on_refill_timer_timeout():
 	refill_columns()
 
 func _on_lock_holder_remove_lock(place):
-	for i in range(lock_spaces.size() - 1, -1, -1):
-		if lock_spaces[i] == place:
-			lock_spaces.remove(i)
+	remove_from_array(lock_spaces, place)
+
+func _on_concrete_holder_remove_concrete(place):
+	remove_from_array(concrete_spaces, place)
