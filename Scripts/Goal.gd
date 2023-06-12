23fdbab6129f2db6a958aa6588c60c7d9c78e530
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-12 09:15:31 +0800

    YT Match 3 P41 - Goal UI

--- Scripts/Goal.gd
+++ Scripts/Goal.gd
@@ -5,6 +5,7 @@ export (Texture) var goal_texture
 export (int) var max_needed
 export (String) var goal_string
 var number_collected = 0
+var goal_met = false
 
 func check_goal(goal_type):
 	if goal_type == goal_string:
@@ -15,5 +16,6 @@ func update_goal():
 	if number_collected < max_needed:
 		number_collected += 1
 	if number_collected == max_needed:
-		print("goal met")
+		if !goal_met:
+			goal_met = true
 
--- Scripts/top_ui.gd
+++ Scripts/top_ui.gd
@@ -1,8 +1,10 @@
 extends TextureRect
 
-onready var score_label = $MarginContainer/HBoxContainer/ScoreLabel
+onready var score_label = $MarginContainer/HBoxContainer/VBoxContainer/ScoreLabel
 onready var counter_label = $MarginContainer/HBoxContainer/CounterLabel
 onready var score_bar = $MarginContainer/HBoxContainer/VBoxContainer/TextureProgress
+onready var goal_container = $MarginContainer/HBoxContainer/HBoxContainer
+export (PackedScene) var goal_prefab
 var current_score = 0
 var current_count = 0
 
@@ -24,5 +26,18 @@ func setup_score_bar(max_score):
 func update_score_bar():
 	score_bar.value = current_score
 
+func make_goal(new_max, new_texture, new_value):
+	var current = goal_prefab.instance()
+	goal_container.add_child(current)
+	current.set_goal_values(new_max, new_texture, new_value)
+
 func _on_grid_setup_max_score(max_score):
 	setup_score_bar(max_score)
+
+func _on_GoalHolder_create_goal(new_max, new_texture, new_value):
+	make_goal(new_max, new_texture, new_value)
+
+func _on_grid_check_goal(goal_type):
+	for i in goal_container.get_child_count():
+		goal_container.get_child(i).update_goal_values(goal_type)
+	pass # Replace with function body.
