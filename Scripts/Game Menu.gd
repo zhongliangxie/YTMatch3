53e3ad9c313bfcfe06684ad4c81b8bf392dc4aa8
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-13 02:03:15 +0800

    YT Match 3 P52 - Config Files

--- Scripts/Game Menu.gd
+++ Scripts/Game Menu.gd
@@ -1,11 +1,14 @@
 extends Control
 
+signal read_sound
+
 func _ready():
 	#Main Menu panel active
 	$Main.slide_in()
 
 
 func _on_Main_settings_pressed():
+	emit_signal("read_sound")
 	$Main.slide_out()
 	$Settings.slide_in()
 
@@ -15,3 +18,6 @@ func _on_Settings_back_button():
 
 func _on_Main_play_pressed():
 	get_tree().change_scene("res://Scenes/LevelSelectScene.tscn")
+
+func _on_Settings_sound_change():
+	ConfigManager.sound_on = !ConfigManager.sound_on
