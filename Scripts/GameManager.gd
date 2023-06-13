0f754916b9aa5d3915ee3015bab2bc0d1798e69e
ZhongLiang Xie <1211961+zhongliangxie@users.noreply.github.com>
2023-06-13 11:43:25 +0800

    YT Match 3 P67 - Refactoring Goals

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -21,6 +21,12 @@ signal set_dimensions
 signal set_score_info
 signal set_counter_info
 
+# Goal Stuff
+onready var goal_holder =$GoalHolder
+var game_won = false
+signal create_goal
+signal game_won
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !is_moves:
@@ -38,7 +44,35 @@ func setup():
 	emit_signal("set_score_info", max_score, current_score)
 	emit_signal("set_dimensions", width, height)
 	emit_signal("set_counter_info", current_counter)
+	create_goals()
 
+func create_goals():
+	for i in goal_holder.get_child_count():
+		var current = goal_holder.get_child(i)
+		emit_signal("create_goal", current.max_needed, current.goal_texture, current.goal_string)
+
+func check_goals(goal_type):
+	for i in goal_holder.get_child_count():
+		goal_holder.get_child(i).check_goal(goal_type)
+	check_game__win()
+
+func check_game__win():
+	if goals_met():
+		emit_signal("game_won")
+		GameDataManager.level_info[level + 1] = {
+			"unlocked": true,
+			"high score": 0,
+			"stars unlocked": 0
+		}
+		game_won = true
+
+func goals_met():
+	for i in goal_holder.get_child_count():
+		if !goal_holder.get_child(i).goal_met:
+			return false
+	return true
+
+# GameManager Signals
 func _on_grid_update_score(streak_value):
 	current_score += streak_value * points_per_piece
 	emit_signal("set_score_info", max_score, current_score)
@@ -52,9 +86,14 @@ func _on_grid_update_counter():
 		emit_signal("set_counter_info", current_counter)
 
 func _on_MoveTimer_timeout():
-	if !is_moves:
+	if !is_moves and !game_won:
 		current_counter -= 1
 		if current_counter < 0:
 			current_counter = 0
 		emit_signal("set_counter_info", current_counter)
-		
+
+func _on_grid_check_goal(goal_type):
+	check_goals(goal_type)
+
+func _on_ice_holder_break_ice(goal_type):
+	check_goals(goal_type)
